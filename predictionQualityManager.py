52cc17f47b9039b3cd3dcd42aad8ef684f78cb0b
Leotis <leotisbuchanan@gmail.com>
2017-03-29 06:46:22 -0400

    adding video processing

--- predictionQualityManager.py
+++ predictionQualityManager.py
@@ -1,10 +1,80 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import cv2
+from scipy.ndimage.measurements import label
+
 class PredictionQualityManager:
 
-    def findBestPredictions(self,detected_cars_bboxes):
+   
+
+    def findBestPredictions(self,heatmap,detected_cars_bboxes):
         """
          attempt to determine if the given boxes are 
          real detecting are false detections
 
         """
+        best_predicted_boxes = [] 
+        # Add heat to each box in box list
+        heat = add_heat(heat,detected_cars_bboxes)
+        
+        # Apply threshold to help remove false positives
+        heat = apply_threshold(heat,1)
 
-        return detected_cars_bboxes
+        # Visualize the heatmap when displaying    
+        heatmap = np.clip(heat, 0, 255)
+
+        # Find final boxes from heatmap using label function
+        labels = label(heatmap)
+
+
+        for car_number in range(1, labels[1]+1):
+            # Find pixels with each car_number label value
+            nonzero = (labels[0] == car_number).nonzero()
+            # Identify x and y values of those pixels
+            nonzeroy = np.array(nonzero[0])
+            nonzerox = np.array(nonzero[1])
+            # Define a bounding box based on min/max x and y
+            bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+            # add boxes  to list 
+            best_predicted_boxes.append(bbox)
+            # Return the image
+        return best_predicted_boxes
+
+
+        
+    def add_heat(self,heatmap, bbox_list):
+        # Iterate through list of bboxes
+        for box in bbox_list:
+            # Add += 1 for all pixels inside each bbox
+            # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+            heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+            # Return updated heatmap
+        return heatmap
+    
+    def apply_threshold(self,heatmap, threshold):
+        # Zero out pixels below the threshold
+        heatmap[heatmap <= threshold] = 0
+        # Return thresholded map
+        return heatmap
+
+    def draw_labeled_bboxes(self,img, labels):
+        # Iterate through all detected cars
+        for car_number in range(1, labels[1]+1):
+            # Find pixels with each car_number label value
+            nonzero = (labels[0] == car_number).nonzero()
+            # Identify x and y values of those pixels
+            nonzeroy = np.array(nonzero[0])
+            nonzerox = np.array(nonzero[1])
+            # Define a bounding box based on min/max x and y
+            bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+            # Draw the box on the image
+            cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+            # Return the image
+        return img
+
+
+
+
--- vehicleDetector.py
+++ vehicleDetector.py
@@ -8,81 +8,104 @@ from windowManager import WindowManager
 from classifier import Classifier
 from settings import Settings
 import sys
+from moviepy.editor import VideoFileClip
+
+class VehicleDetector:
+    def __init__(self):
+        DATA_BASE_PATH = "data/"
+        CARS = "cars1/"
+        NOT_CARS = "notcars1/"
+
+        self.cars_base_path = DATA_BASE_PATH + CARS
+        self.not_cars_base_path = DATA_BASE_PATH + NOT_CARS
+
+        self.test_image = mpimg.imread('data/bbox-example-image.jpg')
+
+            
+    def getImagesPath(self,image_directory):
+        image_paths = glob.glob(image_directory+"/*.jpeg")
+        return image_paths
 
 
-def getImagesPath(image_directory):
-    image_paths = glob.glob(image_directory+"/*.jpeg")
-    return image_paths
+    def loadData(self,base_path):
+        image_paths = self.getImagesPath(base_path)
+        return image_paths
 
 
-def loadData(base_path):
-    image_paths = getImagesPath(base_path)
-    return image_paths
+    def pipeline(self,img):
 
+        # step 1 load the data
+        # step 2 compute features
+        # step 3 train classifier
+        # steo 4 get windows
+        # step 3 prediction
+        # step 4 visualise predictions
 
-def runpipeline():
-
-    # step 1 load the data
-    # step 2 compute features
-    # step 3 train classifier
-    # steo 4 get windows
-    # step 3 prediction
-    # step 4 visualise predictions
-
-    DATA_BASE_PATH = "data/"
-    CARS = "cars1/"
-    NOT_CARS = "notcars1/"
-
-    cars_base_path = DATA_BASE_PATH + CARS
-    not_cars_base_path = DATA_BASE_PATH + NOT_CARS
-
-    test_image = mpimg.imread('data/bbox-example-image.jpg')
-    img = test_image
     
-    settings = Settings()
-    settingsDict = settings.settingsDict
-    featureGenerator = FeatureGenerator(settingsDict)
-    windowManager = WindowManager()
-    classifier = Classifier()
-    predictionQualityManager = PredictionQualityManager()
-    not_cars = loadData(not_cars_base_path)
-    cars = loadData(cars_base_path)
-    car_features = featureGenerator.computeFeatures(cars)
-    non_car_features = featureGenerator.computeFeatures(not_cars)
-    # need to reshape the feature list to be a single
-    # column array
-    classifier.train(car_features, non_car_features)
+        settings = Settings()
+        settingsDict = settings.settingsDict
+        featureGenerator = FeatureGenerator(settingsDict)
+        windowManager = WindowManager()
+        classifier = Classifier()
+        predictionQualityManager = PredictionQualityManager()
+        not_cars = self.loadData(self.not_cars_base_path)
+        cars = self.loadData(self.cars_base_path)
+        car_features = featureGenerator.computeFeatures(cars)
+        non_car_features = featureGenerator.computeFeatures(not_cars)
+        # need to reshape the feature list to be a single
+        # column array
+        classifier.train(car_features, non_car_features)
 
-    # only search the bottom part of the image
-    y_stop = int(test_image.shape[0]*0.7)
-    candidate_windows = windowManager.slide_window(test_image,
+        # only search the bottom part of the image
+        y_stop = int(img.shape[0]*0.7)
+        candidate_windows = windowManager.slide_window(img,
                                                    x_start_stop=[None, None],
                                                    y_start_stop=[y_stop, None], 
                                                    xy_window=(64, 64),
                                                    xy_overlap=(0.5, 0.5))
 
 
-    
 
-    # return a list of boxes coordinates
-    detected_cars_coordinates = windowManager.search_windows(test_image,
+
+        candidate_windows128 = windowManager.slide_window(test_image,
+                                                   x_start_stop=[None, None],
+                                                   y_start_stop=[y_stop, None], 
+                                                   xy_window=(128, 128),
+                                                   xy_overlap=(0.5, 0.5))
+
+    
+        candidate_windows.extend(candidate_windows128)
+
+        # return a list of boxes coordinates
+        detected_cars_coordinates = windowManager.search_windows(test_image,
                                                              candidate_windows,
                                                              classifier,
                                                              featureGenerator,
                                                              settingsDict)
 
 
-    best_pred_bboxes = predictionQualityManager.findBestPredictions(
-        detected_cars_coordinates)
-    output_img = windowManager.draw_boxes(test_image, best_pred_bboxes,
+        best_pred_bboxes = predictionQualityManager.findBestPredictions(
+            detected_cars_coordinates)
+        output_img = windowManager.draw_boxes(test_image, best_pred_bboxes,
                                           color=(0, 0, 255), thick=6)
     
-    plt.imshow(output_img)
-    plt.show()
+        plt.imshow(output_img)
+        plt.show()
 
+
+
+
+    def run(self, videoList):
+        for video in videoList:
+            output =  "output" + video
+            clip2 = VideoFileClip(video)
+            clip = clip2.fl_image(self.pipeline)
+            clip.write_videofile(output, audio=False)    
 
 if __name__ == "__main__":
-    runpipeline()
+    vehicleDetector = VehicleDetector()
+    videoList = ["videos/input/test_video.mp4", "videos/input/project_video.mp4"]
+    vehicleDetector.run(videoList)
     
 
 
